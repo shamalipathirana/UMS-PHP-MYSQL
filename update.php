1a83f4b808863616eefb10a87d599c1c388fbd2c
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-16 14:09:30 +0530

    Create update.php

---
+++ update.php
@@ -0,0 +1,26 @@
+<?php require_once('inc/connection.php'); ?>
+
+
+<?php
+/*UPDATE tabel_name
+  SET column_1=value-1,column_2=value_2
+  WHERE column name=value
+  LIMIT 1
+  */ 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update query</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php  
+mysqli_close($connection );
+?>
